1c02148a2f5005fac98904cc639967dbeb405417
NicolasLemon <NicolasLemon@admin.com>
2023-01-28 15:08:10 +0800

    整合MyBatis-Plus 3.5.1（含代码生成以及自动填充，功能待验证）

---
+++ admin/ruoyi-common/src/main/java/com/ruoyi/common/handler/MybatisPlusMetaObjectHandler.java
@@ -0,0 +1,43 @@
+package com.ruoyi.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.ruoyi.common.utils.SecurityUtils;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * MyBatis-Plus 自动填充字段 处理器
+ *
+ * @author Nicolas·Lemon
+ * @since 2022/05/13
+ */
+@Component
+public class MybatisPlusMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        /// 插入一条记录时的自动填充
+        String thisLoginUsername = SecurityUtils.getLoginUser().getUsername();
+        // 创建者
+        this.setFieldValByName("createBy", thisLoginUsername, metaObject);
+        // 创建时间 起始版本 3.3.3(推荐)
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+        // 更新者
+        this.setFieldValByName("updateBy", thisLoginUsername, metaObject);
+        // 更新时间 起始版本 3.3.3(推荐)
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        // 更新一条记录时的自动填充
+        String thisLoginUsername = SecurityUtils.getLoginUser().getUsername();
+        // 更新者
+        this.setFieldValByName("updateBy", thisLoginUsername, metaObject);
+        // 更新时间 起始版本 3.3.3(推荐)
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+}
